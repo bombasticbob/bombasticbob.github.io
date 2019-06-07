70aa7de951d40be10ee25a4c5f3944f8aa50de18
bob frazier <bobf@mrp3.com>
2019-06-07 04:22:16 -0700

    massive documentation changes (again)

--- X11workbench/group__dlgatom.js
+++
@@ -1,5 +0,0 @@
-var group__dlgatom =
-[
-    [ "aDIALOG_INIT", "group__dlgatom.html#ga463e917d2ec511d683ea32ee5508e2e4", null ],
-    [ "aDLGC_PROP_NOTIFY", "group__dlgatom.html#ga6230272701b633d79a19f9efc5afccd2", null ]
-];
